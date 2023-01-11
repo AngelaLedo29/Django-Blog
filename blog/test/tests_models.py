e39d8f22576c34c5c9b0159a4574153ac92f1bab
AngelaLedo29 <angelaledo29@gmail.com>
2023-01-11 10:36:48 +0100

    Modificación del Tests_Models

--- blog/test/tests_models.py
+++ blog/test/tests_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 from django.contrib.auth.models import User
-from blog.models import Post
+from blog.models import Post, Comment
 
 # Create your tests here.
 class PostModelTest(TestCase):
@@ -10,6 +10,7 @@ class PostModelTest(TestCase):
         # Set up non-modified objects used by all test methods
         u = User.objects.create(first_name='Angela', last_name='Bob')
         p = Post.objects.create(title='My first post', slug='my-first-post', author=u, content='This is my first post content', status=2)
+        c = Comment.objects.create(post=p, name='Angela', email='angela@angela.com', body='This is my first comment')
 
     def test_method_str(self):
         '''
@@ -27,3 +28,11 @@ class PostModelTest(TestCase):
         slug = post.slug
         expected_slug = post.title.lower().replace(' ', '-')
         self.assertEquals(expected_slug, slug)
+
+    def test_str_comment(self):
+        '''
+        Comprobar que el print del objeto devuelve el comentario
+        '''
+        comment = Comment.objects.first()
+        expected_object_name = f'Comment {comment.body} by {comment.name}'
+        self.assertEquals(expected_object_name, str(comment))
